6734c8f95181545e2cf4d674c37f89b0da2ebfd7
devopsbc01 <70231154+devopsbc01@users.noreply.github.com>
2020-08-27 12:47:49 -0700

    Updated IP address

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -16,8 +16,8 @@ public class ftat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://18.218.62.123:8080/QAWebapp/";
-	public String URL1 = "http://18.218.62.123:8080/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://34.121.221.156:8080/QAWebapp/";
+	public String URL1 = "http://34.121.221.156:8080/QAWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
