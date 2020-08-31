c61043ba05f5d446b6b6f1218e311342f270ffc9
devopsbc01 <70231154+devopsbc01@users.noreply.github.com>
2020-08-31 10:50:48 -0700

    updated IP and pass

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -19,7 +19,7 @@ public class createpage extends HttpServlet {
 			Class.forName("org.postgresql.Driver");
 			String jdbcUrl="jdbc:postgresql://34.121.221.156:5432/postgres";
 			String username="postgres";
-			String password="password";
+			String password="";
 		
 						
 			Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
