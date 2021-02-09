ab75e7fdd53bde217504fcfcc4eb9e8456378041
James Ye <998065+zhaoyun95@users.noreply.github.com>
2021-02-08 18:39:46 -0800

    updated ip addresses

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -16,8 +16,8 @@ public class ftat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://3.137.162.240:8080/QAWebapp/";
-	public String URL1 = "http://3.137.162.240:8080/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://172.31.87.233:8080/QAWebapp/";
+	public String URL1 = "http://172.31.87.233:8080/QAWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.137.162.240:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://172.31.87.233:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
